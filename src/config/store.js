6211d6a35a59b4ed5fa81b2a94e02b7bdb04c63b
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2018-11-16 07:20:21 -0500

    add image

--- src/config/store.js
+++ src/config/store.js
@@ -5,4 +5,9 @@ const rootReducer = combineReducers({
   player: playerReducer
 });
 
-const store = createStore;
+const store = createStore(
+  rootReducer /* preloadedState, */,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
