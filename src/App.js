df892c270938c683d174d721b2f827c3ad3d6281
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2018-11-16 08:14:20 -0500

    add map

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import Player from "./features/player";
+import Map from "./features/map";
 class App extends Component {
   render() {
     return (
       <div>
+        <Map />
         <Player />
       </div>
     );
---
+++ src/features/map/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Map(props) {
+  return (
+    <div
+      style={{
+        width: "800px",
+        height: "400px",
+        backgroundColor: "black",
+        border: "4px solid white"
+      }}
+    />
+  );
+}
+
+export default Map;
