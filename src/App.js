c46887e7941009ba5172db634b384595a1a1e775
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2018-11-16 06:54:50 -0500

    create player

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
-
+import Player from "./features/player";
 class App extends Component {
   render() {
-    return <div>Awesome Game</div>;
+    return (
+      <div>
+        <Player />
+      </div>
+    );
   }
 }
 
