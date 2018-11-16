0af626415de9bfecbf4e6ee33d8a5df10940ad65
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2018-11-16 07:03:02 -0500

    create reducer

---
+++ src/config/store.js
@@ -0,0 +1,8 @@
+import { createStore, combineReducers } from "redux";
+import playerReducer from "../features/player/reducer";
+
+const rootReducer = combineReducers({
+  player: playerReducer
+});
+
+const store = createStore;
