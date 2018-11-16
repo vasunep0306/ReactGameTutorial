4da25a424a4cf861b06cfe1a6f15fb5634573e8b
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2018-11-16 07:31:02 -0500

    got player on screen

--- src/features/player/index.js
+++ src/features/player/index.js
@@ -1,7 +1,27 @@
 import React from "react";
+import walkSprite from "./player_walk.png";
+import { connect } from "react-redux";
 
 function Player(props) {
-  return <div>Player</div>;
+  return (
+    <div
+      style={{
+        position: "relative",
+        top: props.position[1],
+        left: props.position[0],
+        backgroundImage: `url(${walkSprite})`,
+        backgroundPosition: "0 0",
+        width: "40px",
+        height: "40px"
+      }}
+    />
+  );
 }
 
-export default Player;
+function mapStateToProps(state) {
+  return {
+    ...state.player
+  };
+}
+
+export default connect(mapStateToProps)(Player);
