f69bc712921fae38c05ea093ae4a1e8c2ed3ee36
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2018-11-16 16:30:07 -0500

    can now move the player

--- src/features/player/movement.js
+++ src/features/player/movement.js
@@ -1,17 +1,39 @@
 import store from "../../config/store";
+import { SPRITE_SIZE } from "../../config/constants";
 
 export default function handleMovement(player) {
+  function getNewPosition(direction) {
+    const oldPos = store.getState().player.position;
+    switch (direction) {
+      case "WEST":
+        return [oldPos[0] - SPRITE_SIZE, oldPos[1]];
+      case "EAST":
+        return [oldPos[0] + SPRITE_SIZE, oldPos[1]];
+      case "NORTH":
+        return [oldPos[0], oldPos[1] - SPRITE_SIZE];
+      case "SOUTH":
+        return [oldPos[0], oldPos[1] + SPRITE_SIZE];
+    }
+  }
+  function dispatchMove(direction) {
+    store.dispatch({
+      type: "MOVE_PLAYER",
+      payload: {
+        position: getNewPosition(direction)
+      }
+    });
+  }
   function handleKeyDown(e) {
     e.preventDefault();
     switch (e.keyCode) {
       case 37:
-        return directionMove("WEST");
+        return dispatchMove("WEST");
       case 38:
-        return directionMove("NORTH");
+        return dispatchMove("NORTH");
       case 39:
-        return directionMove("EAST");
+        return dispatchMove("EAST");
       case 40:
-        return directionMove("SOUTH");
+        return dispatchMove("SOUTH");
       default:
         console.log(e.keyCode);
     }
